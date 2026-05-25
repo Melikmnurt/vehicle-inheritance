038634b8de08700159fec37ded574092ad6747f3
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-25 01:19:06 -0500

    Worked on Vehicle

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
+// Parent class for all vehicle types
 public class Vehicle {
+
+    // The color of the vehicle
+    private String color;
+
+    // Number of passengers the vehicle can hold
+    private int numberOfPassengers;
+
+    // Maximum cargo capacity
+    private int cargoCapacity;
+
+    // Fuel tank capacity
+    private int fuelCapacity;
+
+    // Getter for color
+    public String getColor() {
+        return color;
+    }
+
+    // Setter for color
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Getter for number of passengers
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    // Setter for number of passengers
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    // Getter for cargo capacity
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    // Setter for cargo capacity
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    // Getter for fuel capacity
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    // Setter for fuel capacity
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
 }
